b70b2b2e7c12645a41cf643b416e6863440a7938
maengsanha <msh0117@sogang.ac.kr>
2021-04-18 06:09:10 +0900

    45

--- text_editor.c
+++ text_editor.c
@@ -60,18 +60,20 @@ void text_editor_switch89(struct device_status *status) {
  * @status: current status of the device
  */
 void handle_text_editor(struct device_status *status) {
-  text_editor_update_switch_cnt(status);
   if (status->switch_val[1] == KEY_PRESS && status->switch_val[2] == KEY_PRESS) {
+    text_editor_update_switch_cnt(status);
     text_editor_switch23(status);
     return;
   }
 
   if (status->switch_val[4] == KEY_PRESS && status->switch_val[5] == KEY_PRESS) {
+    text_editor_update_switch_cnt(status);
     text_editor_switch56(status);
     return;
   }
 
   if (status->switch_val[7] == KEY_PRESS && status->switch_val[8] == KEY_PRESS) {
+    text_editor_update_switch_cnt(status);
     text_editor_switch89(status);
     return;
   }
@@ -79,12 +81,13 @@ void handle_text_editor(struct device_status *status) {
   unsigned int i;
   for (i=0; i<9; ++i) {
     if (status->switch_val[i] == KEY_PRESS) {
+      text_editor_update_switch_cnt(status);
       if (status->mode_3_mode == NUMBER_MODE) {
         if (status->mode_3_write_pos == 31) {
           memcpy(status->text_lcd_val, &status->text_lcd_val[1], 31);
-          status->text_lcd_val[status->mode_3_write_pos] = i+1;
+          status->text_lcd_val[status->mode_3_write_pos] = i+49;
         } else {
-          status->text_lcd_val[status->mode_3_write_pos] = i+1;
+          status->text_lcd_val[status->mode_3_write_pos] = i+49;
           status->mode_3_write_pos++;
         }
       } else {
