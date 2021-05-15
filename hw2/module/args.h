99760a71497e3e4f20d20b255b33c96d37c9f26e
maengsanha <msh0117@sogang.ac.kr>
2021-05-16 03:17:57 +0900

    19

--- hw2/module/args.h
+++ hw2/module/args.h
@@ -20,10 +20,4 @@ struct args {
  *
  * @param: command line argument from user program
  */
-inline unsigned int get_init_val(struct args *param) {
-  unsigned int init = param->init;
-  if (0 < init/1000) return init/1000;
-  if (0 < init/100)  return init/100;
-  if (0 < init/10)   return init/10;
-  return init;
-}
+inline unsigned int get_init_val(struct args *param);
