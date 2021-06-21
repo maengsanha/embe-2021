1f910e5b4791af2a3956db510a684e73a321ac37
maengsanha <msh0117@sogang.ac.kr>
2021-06-22 06:49:12 +0900

    26

--- project/jni/topinfo_test.c
+++ project/jni/topinfo_test.c
@@ -12,16 +12,12 @@
 #define BUFSIZE   16384
 
 char *get_process_info() {
-  pid_t pid;
-  if ((pid = fork()) == -1) {
-    printf("fork failed\n");
-    exit(1);
-  } else if (pid == 0) {
+  if (fork() == 0) {
     execlp("sh", "sh", "-c", "top -n 1 > /data/local/tmp/output.txt");
   } else {
     wait(NULL);
 
-    char *buf = malloc(sizeof(char)*BUFSIZE);
+    char *buf = malloc(BUFSIZE);
 
     int fd;
     if ((fd = open(FILENAME, O_RDONLY)) < 0) {
@@ -32,38 +28,39 @@ char *get_process_info() {
 
     read(fd, buf, BUFSIZE);
     close(fd);
+
+    struct sys_info_t info = {
+      .user_usage = -1,
+      .sys_usage = -1,
+    };
+
+    char *token = strtok(buf, "%%");
+    char ustr[strlen(token)];
+    strcpy(ustr, token);
+
+    token = strtok(NULL, "%%");
+    char sstr[strlen(token)];
+    strcpy(sstr, token);
+
+    char *_token = strtok(ustr, " ");
+    _token = strtok(NULL, " ");
+    int user_usage = atoi(_token);
+
+    char *__token = strtok(sstr, " ");
+    __token = strtok(sstr, " ");
+    __token = strtok(sstr, " ");
+    int sys_usage = atoi(__token);
+
+    syscall(376, &info, &user_usage, &sys_usage);
+
+    printf("User: %d\n", info.user_usage);
+    printf("System: %d\n", info.sys_usage);
+
     return buf;
   }
 }
 
 int main() {
   char *buf = get_process_info();
-
-  struct sys_info_t info = {
-    .user_usage = -1,
-    .sys_usage = -1,
-  };
-
-  char *token = strtok(buf, "%%");
-  char ustr[strlen(token)];
-  strcpy(ustr, token);
-
-  token = strtok(NULL, "%%");
-  char sstr[strlen(token)];
-  strcpy(sstr, token);
-
-  char *_token = strtok(ustr, " ");
-  _token = strtok(NULL, " ");
-  int user_usage = atoi(_token);
-
-  char *__token = strtok(sstr, " ");
-  __token = strtok(sstr, " ");
-  __token = strtok(sstr, " ");
-  int sys_usage = atoi(__token);
-
-  syscall(376, &info, &user_usage, &sys_usage);
   free(buf);
-
-  printf("User: %d\n", info.user_usage);
-  printf("System: %d\n", info.sys_usage);
 }
