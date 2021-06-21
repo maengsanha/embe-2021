b9bf3e9de2fab8716229aa7d50f1eb83254260b1
maengsanha <msh0117@sogang.ac.kr>
2021-06-21 18:28:43 +0900

    3

--- project/jni/topinfo_test.c
+++ project/jni/topinfo_test.c
@@ -3,19 +3,30 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <syscall.h>
+#include <sys/wait.h>
 
 #define FILENAME  "/data/local/tmp/output.txt"
 #define BUFSIZE   32768
 
 char *get_process_info() {
-  execlp("/system/bin/sh", "/system/bin/sh", "-c", "top -n 1 > /data/local/tmp/output.txt");
-  char *buf = malloc(sizeof(char)*BUFSIZE);
-  int fd;
-  if ((fd = open(FILENAME, O_RDONLY)) < 0) {
-    printf("open failed :%d\n", fd);
-    exit(1);
+  pid_t pid;
+  char *buf;
+
+  switch (pid = fork()) {
+    case 0:
+      execlp("/system/bin/sh", "/system/bin/sh", "-c", "top -n 1 > /data/local/tmp/output.txt");
+      break;
+    default:
+      wait(NULL);
+      buf = malloc(sizeof(char)*BUFSIZE);
+      int fd;
+      if ((fd = open(FILENAME, O_RDONLY)) < 0) {
+        printf("open failed :%d\n", fd);
+        exit(1);
+      }
+      read(fd, buf, BUFSIZE);
+      break;
   }
-  read(fd, buf, BUFSIZE);
   return buf;
 }
 
