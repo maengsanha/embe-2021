eb59646340ba5ef7b23cbccc9e671e52629cfa56
maengsanha <msh0117@sogang.ac.kr>
2021-06-22 07:03:59 +0900

    30

--- project/jni/topinfo_test.c
+++ project/jni/topinfo_test.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <syscall.h>
-#include <wait.h>
+#include <sys/wait.h>
 
 #include "sysinfo.h"
 
@@ -12,50 +12,54 @@
 #define BUFSIZE   16384
 
 char *get_process_info() {
-  execlp("sh", "sh", "-c", "top -n 1 > /data/local/tmp/output.txt");
-  wait(NULL);
+  if (fork() == 0) {
+    execlp("sh", "sh", "-c", "top -n 1 > /data/local/tmp/output.txt");
+    exit(0);
+  } else {
+    wait(NULL);
 
-  char *buf = malloc(BUFSIZE);
+    char *buf = malloc(BUFSIZE);
 
-  int fd;
-  if ((fd = open(FILENAME, O_RDONLY)) < 0) {
-    printf("open failed: %d\n", fd);
-    free(buf);
-    exit(1);
+    int fd;
+    if ((fd = open(FILENAME, O_RDONLY)) < 0) {
+      printf("open failed: %d\n", fd);
+      free(buf);
+      exit(1);
+    }
+
+    read(fd, buf, BUFSIZE);
+    close(fd);
+
+    struct sys_info_t info = {
+      .user_usage = -1,
+      .sys_usage = -1,
+    };
+
+    char *token = strtok(buf, "%%");
+    char ustr[strlen(token)];
+    strcpy(ustr, token);
+
+    token = strtok(NULL, "%%");
+    char sstr[strlen(token)];
+    strcpy(sstr, token);
+
+    char *_token = strtok(ustr, " ");
+    _token = strtok(NULL, " ");
+    int user_usage = atoi(_token);
+
+    char *__token = strtok(sstr, " ");
+    __token = strtok(sstr, " ");
+    __token = strtok(sstr, " ");
+    int sys_usage = atoi(__token);
+
+    syscall(376, &info, &user_usage, &sys_usage);
+
+    // write to device driver
+    printf("User: %d\n", info.user_usage);
+    printf("System: %d\n", info.sys_usage);
+
+    return buf;
   }
-
-  read(fd, buf, BUFSIZE);
-  close(fd);
-
-  struct sys_info_t info = {
-    .user_usage = -1,
-    .sys_usage = -1,
-  };
-
-  char *token = strtok(buf, "%%");
-  char ustr[strlen(token)];
-  strcpy(ustr, token);
-
-  token = strtok(NULL, "%%");
-  char sstr[strlen(token)];
-  strcpy(sstr, token);
-
-  char *_token = strtok(ustr, " ");
-  _token = strtok(NULL, " ");
-  int user_usage = atoi(_token);
-
-  char *__token = strtok(sstr, " ");
-  __token = strtok(sstr, " ");
-  __token = strtok(sstr, " ");
-  int sys_usage = atoi(__token);
-
-  syscall(376, &info, &user_usage, &sys_usage);
-
-  // write to device driver
-  printf("User: %d\n", info.user_usage);
-  printf("System: %d\n", info.sys_usage);
-
-  return buf;
 }
 
 int main() {
